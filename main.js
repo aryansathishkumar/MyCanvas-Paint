59bcfb7a0e4a8aac598cfc0ca1828d9198a1cfbb
aryansathishkumar <76153434+aryansathishkumar@users.noreply.github.com>
2021-02-07 19:33:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ var current_x, current_y
 var mouseEvent = "empty"
 color = "black"
 width = 2
+radius = 36
 canvas.addEventListener("mousedown", mouse_down)
 function mouse_down(e){
     color = document.getElementById("color").value
     width = document.getElementById("width_of_line").value
+    radius = document.getElementById("radius").value
    mouseEvent = "mouse_down"
 }
 canvas.addEventListener("mousemove", mouse_move)
@@ -16,14 +18,13 @@ function mouse_move(e){
 
    if(mouseEvent == "mouse_down"){
      console.log("mouse move starts") 
-     ctx.beginPath()
-     ctx.strokeStyle = color
-     ctx.lineWidth = width
      ctx.moveTo(last_x, last_y)
      current_x = e.clientX - canvas.offsetLeft
      current_y = e.clientY - canvas.offsetTop
      console.log(current_x, current_y)
-     ctx.lineTo(current_x, current_y)
+     ctx.beginPath()
+     ctx.strokeStyle = color
+     ctx.arc(current_x, current_y, radius, 0, Math.PI*2)
      ctx.stroke()
    }
    last_x = current_x
